feddba3596f05e4f8815b8f00b90b8d1f300c179
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2025-04-27 16:03:54 +0300

    feat: add global pref, enablaedCors and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,14 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://31.128.48.25:3000'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
