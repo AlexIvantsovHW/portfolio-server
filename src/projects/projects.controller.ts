968fc3d34b4f1f644002b707029d8c13e679a869
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2025-05-06 14:39:00 +0300

    feat: update findAll and findOne project service returning types

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -14,12 +14,6 @@ import { UpdateProjectDto } from './dto/update-project.dto';
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
-
- /*  @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectsService.create(createProjectDto);
-  }
- */
   @Get()
   findAll() {
     return this.projectsService.findAll();
@@ -38,23 +32,8 @@ export class ProjectsController {
     return this.projectsService.delete(+id)
   }
   @Patch(':id')
-  update(@Param('id')id:string,@Body()updateProjectDto:CreateProjectDto){
+  update(@Param('id')id:string,@Body()updateProjectDto:UpdateProjectDto){
     return this.projectsService.update(+id,updateProjectDto)
   }
-  
 
- /*  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectsService.update(+id, updateProjectDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectsService.remove(+id);
-  } */
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,18 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { ProjectEntity } from './entities/project.entity';
-import { ProjectRepository } from './projects.repository';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { IProjects } from './module/projects.interface';
 
 @Injectable()
 export class ProjectsService {
     constructor(private prisma: PrismaService) {}
   
-  async findAll() {
-    return this.prisma.projects.findMany();  // Используем Prisma для запроса
+  async findAll():Promise<ProjectEntity[]> {
+    const projects=await this.prisma.projects.findMany(); 
+    const dateTransformProjects=projects.map((project,idx)=>{return({...project,startAt:project.startAt.toString(),endAt:project.endAt.toString()})})
+     return dateTransformProjects
   }
-  async findOne(id:number){
-    return this.prisma.projects.findUnique({where:{id:id}})
+  async findOne(id:number):Promise<IProjects|{message:string}>{
+   const project= await this.prisma.projects.findUnique({where:{id:id}})
+   if(!project){
+    return {message:`Project with id ${id} doesn't exist in DB`}
+   }
+    const dateTransformProject={...project,startAt:project?.startAt.toString(),endAt:project?.endAt.toString()}
+    return dateTransformProject
+    
   }
   async create(createProjectDto:CreateProjectDto) {
     return this.prisma.projects.create({
@@ -30,27 +38,6 @@ async delete(id:number){
 async update(id:number,updateProjectDto: UpdateProjectDto){
   const project=await this.prisma.projects.findUnique({where:{id}})
   return !project||project===null?{message:`Project with id ${id} doesn't exist in DB`}:this.prisma.projects.update({where:{id},data:updateProjectDto})
-
 }
   
-  //constructor(private readonly projectsRepository: ProjectRepository) {}
-  /* create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
-  }
-
-  findAll(): Promise<ProjectEntity[]> {
-    return this.projectsRepository.getAllProjects();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
-  }
-
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} project`;
-  } */
 }
