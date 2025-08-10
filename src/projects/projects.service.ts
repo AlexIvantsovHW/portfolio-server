7631db8153a57813b285c6597fe876a6f602e749
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2025-08-10 21:36:32 +0300

    feat:update delete project service logic

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,10 +2,11 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { ProjectEntity } from './entities/project.entity';
-import { IProjects } from './module/projects.interface';
+import { IProjects, Tproject } from './module/projects.interface';
 import { PrismaService } from '../prisma/prisma.service';
 import { MessageDto } from './dto/message.dto';
 import { handlePrismaError } from 'src/exception/prisma-error-handler/prisma-error-handler';
+import { Tresponse } from 'src/utilts/models/response.type';
 
 @Injectable()
 export class ProjectsService {
@@ -38,38 +39,57 @@ export class ProjectsService {
   }
   async create(
     createProjectDto: CreateProjectDto,
-  ): Promise<ProjectEntity | MessageDto> {
-    const chechingProject = await this.prisma.projects.findFirst({
-      where: { title: createProjectDto.title },
-    });
-    if (chechingProject || chechingProject != null)
-      return {
-        message: `Project: ${createProjectDto.title} is existing in DB!`,
+  ): Promise<Tresponse<Tproject>> {
+    try {
+      const chechingProject = await this.prisma.projects.findFirst({
+        where: { title: createProjectDto.title },
+      });
+      if (chechingProject || chechingProject != null)
+        return {
+          message: `Project: ${createProjectDto.title} is existing in DB!`,
+        };
+
+      const newProject = await this.prisma.projects.create({
+        data: {
+          ...createProjectDto,
+        },
+      });
+      const transformedData = {
+        ...newProject,
+        startAt: newProject.startAt.toString(),
+        endAt: newProject.endAt.toString(),
       };
 
-    const newProject = await this.prisma.projects.create({
-      data: {
-        ...createProjectDto,
-      },
-    });
-    const transformedData = {
-      ...newProject,
-      startAt: newProject.startAt.toString(),
-      endAt: newProject.endAt.toString(),
-    };
-
-    return transformedData;
+      return {
+        data: transformedData,
+        message: 'Project was successfully created!',
+      };
+    } catch (e) {
+      handlePrismaError(e);
+    }
   }
-  async delete(id: number): Promise<CreateProjectDto | MessageDto> {
-    const project = await this.prisma.projects.findUnique({ where: { id } });
-    if (!project || project === null)
-      return { message: `Project with id ${id} doesn't exist in DB` };
-    const deletedProject = await this.prisma.projects.delete({ where: { id } });
-    return {
-      ...deletedProject,
-      startAt: deletedProject.startAt.toString(),
-      endAt: deletedProject.endAt.toString(),
-    };
+  async delete(id: number): Promise<Tresponse<IProjects[]>> {
+    try {
+      const project = await this.prisma.projects.findUnique({ where: { id } });
+      if (!project || project === null)
+        return { message: `Project with id ${id} doesn't exist in DB` };
+      await this.prisma.projects.delete({
+        where: { id },
+      });
+      const updatedData = (await this.prisma.projects.findMany()).map((p) => {
+        return {
+          ...p,
+          startAt: p.startAt.toString(),
+          endAt: project.endAt.toString(),
+        };
+      });
+      return {
+        data: updatedData,
+        message: 'Project was successfully deleted!',
+      };
+    } catch (e) {
+      handlePrismaError(e);
+    }
   }
   async update(
     id: number,
